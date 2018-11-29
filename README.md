ca4b14c84ffba3e156c9b501100790ab655c3136
Joel Wineland <jawineland@outlook.com>
2018-11-29 14:13:21 -0600

    async pbkdf2 example added

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-scratchpad
+
+Repository for code snippets and examples to test certain concepts and to have them for future reference.
---
+++ promisify-pkdf2.js
@@ -0,0 +1,40 @@
+// crypo.randomBytes() and crypto.pbkdf2() are CPU intensive. The synchronous versions may block the worker pool with a large number of requests
+// use the built-in util.promisify method to convert the callback-based functions into promises
+
+const util = require('util')
+const crypto = require('crypto')
+
+// promisify crypto.pbkdf2() and crypto.randomBytes()
+const pbkdf2Async = util.promisify(crypto.pbkdf2)
+const randomBytesAsync = util.promisify(crypto.randomBytes)
+
+
+function generatePassword(password, saltLen=32, iter=50000, keyLen=256, digest='sha256') {
+    
+    this.exportedAuth = {} // object will be populated with salt and hash once computed
+    return randomBytesAsync(saltLen)
+            .then((buffer) => {
+                let salt = buffer.toString('hex')
+                console.log(`The salt is: ${ salt }\n`) // logs salt correctly
+                this.exportedAuth.salt = salt                
+                return pbkdf2Async(password, salt, iter, keyLen, digest)
+            })
+            .then((hash) => {
+                hash = hash.toString('hex')
+                console.log(`The hash is: ${ hash }\n`) // logs hash correctly
+                this.exportedAuth.hash = hash
+                console.log(this.exportedAuth) // logs {salt: 'XXXX', hash: 'XXXX'}
+                // exportedAuth object can be awaited by other code to insert into a DB
+            })
+            .catch((err) => {
+                console.error(err)
+            })
+}
+
+generatePassword('sup3r_s3cur3')
+
+
+
+module.exports = {
+    generatePassword
+}
